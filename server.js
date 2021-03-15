12140af18a070adf6f5d6dd42505e30854661602
negasimichael <negasimichael1@gmail.com>
2021-03-15 16:49:33 -0500

    deploying

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan("dev"));
-require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-var  uri = process.env.MONGODB_URI 
+var uri = process.env.MONGODB_URI
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
+.then(()=> console.log('mongodb connected'))
+.catch(error => console.log('mongodb connection', error));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
